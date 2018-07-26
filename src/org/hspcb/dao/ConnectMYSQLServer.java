df4bffd1abe3cecbb2c86d92fdba8dd5b3083e03
Ashi <Ashi@IT>
2018-07-26 15:40:16 +0530

    UPDATED FILES

--- src/org/hspcb/dao/ConnectMYSQLServer.java
+++ src/org/hspcb/dao/ConnectMYSQLServer.java
@@ -2,14 +2,12 @@ package org.hspcb.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
 public class ConnectMYSQLServer {
 
-	private String db_connect_string = "jdbc:mysql://localhost:3306/";
-	private String db_userid = "root";
-	private String db_password = "Admin@1234";
+	private  final String db_connect_string = "jdbc:mysql://localhost:3306/";
+	private  final String db_userid = "root";
+	private final String db_password = "Admin@1234";
 
 	public Connection dbConnect() {
 		Connection con = null;
@@ -17,11 +15,10 @@ public class ConnectMYSQLServer {
 			Class.forName("com.mysql.jdbc.Driver");
 			con = DriverManager.getConnection(db_connect_string, db_userid, db_password);
 			System.out.println("DB Connected");
-			// here sonoo is database name, root is username and password
 		} catch (Exception e) {
 			System.out.println(e);
 		}
-		
+
 		return con;
 	}
 }
--- src/org/hspcb/service/Service.java
+++ src/org/hspcb/service/Service.java
@@ -1,7 +1,10 @@
-package org.hspcb.contoller;
+package org.hspcb.service;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.hspcb.bean.UserData;
 import org.hspcb.dao.ConnectMYSQLServer;
 
 public class Service {
@@ -24,5 +27,40 @@ public class Service {
 			System.out.println(e);
 		}
 		return status;
+	}
+
+	public void  addUserDts(UserData userData) throws SQLException{
+		System.out.println("Adding");
+		String query="INSERT INTO hspcb.SRODATA values (?,?,?)";
+		System.out.println("Getting DB Connection");
+		ConnectMYSQLServer connectMYSQLServer = new ConnectMYSQLServer();
+		Connection connection = connectMYSQLServer.dbConnect();
+		PreparedStatement pstatement=connection.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS);
+System.out.println("1"+query);
+		String name=userData.getUsername();
+		String pwd=userData.getPassword();
+		pstatement.setString(1, null);
+		pstatement.setString(2, name);
+		pstatement.setString(3, pwd);
+
+		System.out.println("2"+query);
+		int count =pstatement.executeUpdate();
+		System.out.println("count"+count);
+	}
+	public List<UserData> getDepatmentlist() throws SQLException{
+		String query="select * from hspcb.user_info";
+		ConnectMYSQLServer connectMYSQLServer = new ConnectMYSQLServer();
+		Connection connection = connectMYSQLServer.dbConnect();
+		PreparedStatement pstatement=connection.prepareStatement(query);
+		ResultSet rset=pstatement.executeQuery();
+		List<UserData> userInfo = new ArrayList<UserData>();
+		while(rset.next()){
+			UserData userData= new UserData();
+			
+			userData.setUsername(rset.getString(1));
+			userData.setPassword(rset.getString(2));
+			userInfo.add(userData);
+		}
+		return userInfo;
 	}
 }
