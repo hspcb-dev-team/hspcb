b1d0e953e1db5f2e58eae9fdf07017176f6975b3
hspcb.dev.team <hspcb.dev.team@gmail.com>
2018-07-26 16:04:32 +0530

    cleaning up the mess here

--- src/org/hspcb/servlets/LoginServlet.java
+++ src/org/hspcb/servlets/LoginServlet.java
@@ -12,11 +12,9 @@ import org.hspcb.service.Service;
 public class LoginServlet extends HttpServlet {
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+//		request.getRequestDispatcher("Welcome.jsp").forward(request, response);
 		response.setContentType("text/html");
 
-		request.getRequestDispatcher("Welcome.jsp").forward(request, response);
-		/*response.setContentType("text/html");
->>>>>>> refs/remotes/origin/master
 		PrintWriter out = response.getWriter();
 
 		String n = request.getParameter("_ctl0:ContentPlaceHolder1:txtusername");
@@ -25,17 +23,14 @@ public class LoginServlet extends HttpServlet {
 		System.out.println("User: " + n + " Name" + p);
 
 		if (Service.validate(n, p)) {
-			// String name = request.getParameter("username");
-			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/home.jsp");
+			request.getRequestDispatcher("/WEB-INF/views/welcome.jsp").forward(request, response);;
 			out.print("Welcome " + n);
-			rd.forward(request, response);
 		} else {
 			String final_resp ="<html><head><body><h1><font color:red>User/Password does not match!!!</font></h1></body></head></html>";
 			out.print(final_resp);			
-			RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
-			rd.include(request, response);
+			request.getRequestDispatcher("index.jsp").include(request, response);
 		}
 
-		out.close();*/
+		out.close();
 	}
 }
