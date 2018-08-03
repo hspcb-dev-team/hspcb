b8da4bdf679733d6518705d0f7edd1964a0e2344
Ashi <Ashi@IT>
2018-08-03 10:19:30 +0530

    Sync

---
+++ src/org/hspcb/bean/UserProfile.java
@@ -0,0 +1,100 @@
+package org.hspcb.bean;
+
+import java.sql.Date;
+
+public class UserProfile {
+
+	private int upId;
+	private String firstName;
+	private String lastName;
+	private String createdBy;
+	private String employeeId;
+	private String designation;
+	private String department;
+	private String status;
+	private String mobileNo;
+	private String contactNo;
+	private String emailId;
+	private Date lastUpdated;
+	private String userRole;
+	
+	public int getUpId() {
+		return upId;
+	}
+	public void setUpId(int upId) {
+		this.upId = upId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+	public String getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(String employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	public String getContactNo() {
+		return contactNo;
+	}
+	public void setContactNo(String contactNo) {
+		this.contactNo = contactNo;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public Date getLastUpdated() {
+		return lastUpdated;
+	}
+	public void setLastUpdated(Date lastUpdated) {
+		this.lastUpdated = lastUpdated;
+	}
+	public String getUserRole() {
+		return userRole;
+	}
+	public void setUserRole(String userRole) {
+		this.userRole = userRole;
+	}
+	
+}
